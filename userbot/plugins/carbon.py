0e3a3cb5b065c092f0df4ce336cb0e60c3cfde85
vipinpkd <54353789+vipinpkd@users.noreply.github.com>
2020-05-29 22:04:43 +0530

    Update carbon.py

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -152,7 +152,7 @@ async def carbon_api(e):
 
          file,
 
-         caption="<< Here's your carbon, \n Carbonised by [GujjuBot](https://www.github.com/japarmar/GujjuBot)>> ",
+         caption="<< Here's your carbon, \n >> ",
 
          force_document=True,
 
